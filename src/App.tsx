53adf7511f9d2f57f984301124f17e7b6d36209f
Marianne Göschl <maria.anna@goeschl.bayern>
2024-10-24 10:01:10 +0200

    Heading component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import { Button } from './components/Button';
+import { Button } from "./components/Button";
+import { Heading } from "./components/Heading";
 
 function App() {
   return (
     <main className="flex flex-col items-center gap-5 p-10">
-      <h1 className="text-3xl font-bold">React Components (Overview)</h1>
+      <Heading as="h1" className="text-3xl font-bold">
+        React Components (Overview)
+      </Heading>
       <Button className="bg-slate-500" type="button">
         I am a button
       </Button>
